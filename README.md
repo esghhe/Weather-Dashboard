439fd0a3fb85ef4c30783f62a53e8ac8923a89d8
Aden Adhanom <adenadhanom@Adens-MacBook-Air.local>
2022-02-03 13:14:37 -0500

    make some changes to the html file and i started ti write thejs file

--- README.md
+++ README.md
@@ -11,4 +11,4 @@
 
 ## Demo
 
-## Contributor
+## Contributor 
---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+// starting the script page
+function initPage() {
+    const cityEl = document.getElementById("enter-city");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("current-temp");
+    const currentHumidityEl = document.getElementById("current-humidity");
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const currenthistoryEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("today-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+    // assigning uniqe API to the variable
+    const APIKey = "dfaf0a99489f7789af6d598fabccc5cd";
+
+    function getWeather(cityName) {
+        // Execute a current weather get request from open weather api
+let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+axios.get(queryURL)
+.then(function(response) {
+
+    todayweatherEl.classList.remove("d-none");
+
+    // parse response to display current weather
+    const currentDate = new Date(response.data.dt * 1000);
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+    nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+    let weatherPic = response.data.weather[0].icon;
+    currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+    currentPicEl.setAttribute("alt", response.data.weather[0].description);
+    currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+    currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+    currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
+
+    // lets get UV index
+    
+})
+    }
+}
